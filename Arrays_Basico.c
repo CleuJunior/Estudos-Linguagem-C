9e393ad2854d0502f70a1f044ecf10260946c7c2
CleuJunior <cleu.junior@gmail.com>
2020-05-17 21:39:16 -0300

    Print básico de um array de números inteiros.

---
+++ Arrays_Basico.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main(void){
+
+    int Array[7] = {3,4,1,8,9,5,6};
+
+    for(int index = 0; index < 7; index++){
+
+        printf("%d ", Array[index]);
+
+    }
+
+    return 0;
+}
